a6fd39c66204df9b11251031daaacb6f97468bc0
dongbin <dong.firstname@qq.com>
2020-12-30 16:49:31 +0800

    test

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author db
+ * @date 2020/12/30 - 16:39
+ */
+@RestController
+@RefreshScope   //springcloud 原生态注解，实现配置的自动更新
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
+
+

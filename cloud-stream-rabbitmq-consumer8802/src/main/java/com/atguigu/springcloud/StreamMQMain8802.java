9425baedcfbc67f2e1fed22dec8b203534f58e10
dongbin <dong.firstname@qq.com>
2020-12-30 13:09:31 +0800

    test

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/atguigu/springcloud/StreamMQMain8802.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author db
+ * @date 2020/12/30 - 11:24
+ */
+@SpringBootApplication
+public class StreamMQMain8802 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8802.class, args);
+    }
+
+}
+

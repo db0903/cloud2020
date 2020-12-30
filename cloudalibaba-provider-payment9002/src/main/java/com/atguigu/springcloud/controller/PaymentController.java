4cba73be019e3193e6695b135b96b18d9681ded1
dongbin <dong.firstname@qq.com>
2020-12-30 16:05:24 +0800

    test

---
+++ cloudalibaba-provider-payment9002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @author db
+ * @date 2020/12/30 - 16:02
+ */
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Integer id) {
+        return "nacos registry, serverPort: "+ serverPort+"\t id"+id;
+    }
+}

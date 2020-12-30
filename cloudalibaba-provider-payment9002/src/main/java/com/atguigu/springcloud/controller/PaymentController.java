0ba7501549cbd7d7e6b9f2c3f2cb9f51897fd390
dongbin <dong.firstname@qq.com>
2020-12-30 16:32:56 +0800

    test

--- cloudalibaba-provider-payment9002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloudalibaba-provider-payment9002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -1,13 +1,17 @@
 package com.atguigu.springcloud.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author db
  * @date 2020/12/30 - 16:02
  */
+@RestController
+@Slf4j
 public class PaymentController {
     @Value("${server.port}")
     private String serverPort;

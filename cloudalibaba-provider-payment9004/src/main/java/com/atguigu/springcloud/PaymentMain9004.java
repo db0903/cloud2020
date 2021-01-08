f61ac44488922013859a06dc7a1b13184ef4fd0b
dongbin <dong.firstname@qq.com>
2021-01-08 16:58:49 +0800

    test

---
+++ cloudalibaba-provider-payment9004/src/main/java/com/atguigu/springcloud/PaymentMain9004.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author db
+ * @date 2021/1/8 - 16:48
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9004.class, args);
+    }
+
+}
